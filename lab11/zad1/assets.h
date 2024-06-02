b230894bd74fe0556ba9be7baf3873d39f524296
K1Tony <antek.kuchar@gmail.com>
2024-06-02 10:25:14 +0200

    Adding multiple people works

--- lab11/zad1/assets.h
+++ lab11/zad1/assets.h
@@ -1,7 +1,7 @@
 #ifndef ASSETS_H
 #define ASSETS_H
 
-#define MAX_BACKLOG 5
+#define MAX_BACKLOG 2
 #define MAX_CLIENT_NAME_SIZE 256
 
 #define MAX_MSG_LENGTH 1024
@@ -18,13 +18,14 @@ typedef struct message {
 
 typedef struct client {
     int sockfd;
+    int client_id;
     char name[MAX_CLIENT_NAME_SIZE];
-    struct sockaddr_in addr;
+    struct sockaddr_in *addr;
 } client_t;
 
 typedef struct server {
     int sockfd;
-    struct sockaddr_in addr;
+    struct sockaddr_in *addr;
 } server_t;
 
 #endif // ASSETS_H
--- lab11/zad1/server.c
+++ lab11/zad1/server.c
@@ -23,7 +23,7 @@ volatile int run = 1;
 int server_sockfd;
 int client_sockfd[MAX_BACKLOG];
 
-size_t addr_size = sizeof(struct sockaddr_in);
+socklen_t addr_size = sizeof(struct sockaddr_in);
 
 int free_socket = 0;
 
@@ -32,6 +32,10 @@ client_t clients[MAX_BACKLOG];
 int client_count = 0;
 
 char welcome[MAX_MSG_LENGTH] = {0};
+
+// threading
+pthread_t acceptor_thread;
+pthread_t receiver_threads[MAX_BACKLOG];
 
 void handle(int signum) {
     for (size_t i = 0; i < MAX_BACKLOG; i++) {
@@ -43,7 +47,33 @@ void handle(int signum) {
 }
 
 void *acceptor_thread_fun(void *arg) {
-    
+    server_t *serv = (server_t *) arg;
+    while (run) {
+        int new_socket = accept(serv->sockfd, (struct sockaddr *) serv->addr, &addr_size);
+        if (client_count == MAX_BACKLOG) {
+            send(new_socket, "Sorry, chat is full :<", MAX_MSG_LENGTH - 1, 0);
+            continue;
+        }
+        client_count++;
+        clients[free_socket].sockfd = new_socket;
+        clients[free_socket].client_id = free_socket;
+        recv(new_socket, clients[free_socket].name, MAX_CLIENT_NAME_SIZE - 1, 0);
+
+        sprintf(welcome, "Welcome to the chat %s!", clients[free_socket].name);
+        send(new_socket, welcome, MAX_MSG_LENGTH - 1, 0);
+        memset(welcome, 0, sizeof(welcome));
+
+        sprintf(welcome, "%s has entered the chat!", clients[free_socket].name);
+        for (int i = 0; i < MAX_BACKLOG; i++) {
+            if (i != free_socket) {
+                send(clients[i].sockfd, welcome, MAX_MSG_LENGTH - 1, 0);
+            }
+        }
+
+        free_socket++;
+        free_socket %= MAX_BACKLOG;
+    }
+    return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -66,6 +96,12 @@ int main(int argc, char **argv) {
     socklen_t client_addr_sizes[MAX_BACKLOG];
 
     message_t client_messages[MAX_BACKLOG];
+
+    server_t serv;
+    serv.sockfd = server_sockfd;
+    serv.addr = &sa_addr;
+
+    pthread_create(&acceptor_thread, NULL, acceptor_thread_fun, (void *) &serv);
 
     for (short i = 0; i < MAX_BACKLOG; i++) {
         client_sockfd[i] = -1;
@@ -92,34 +128,32 @@ int main(int argc, char **argv) {
 
     // Ctrl+C
     signal(SIGINT, handle);
-
-    int free_addr = 0;
-    char welcome[MAX_MSG_LENGTH + MAX_CLIENT_NAME_SIZE];
     
-    while (run) {
-        int new_socket = accept(server_sockfd, (struct sockaddr *) &sa_addr, client_addr_sizes + free_addr);
-        if (!run) break;
-        if (new_socket >= 0) {
-            printf("ACCEPTED\n");
-            recv(new_socket, welcome, MAX_CLIENT_NAME_SIZE - 1, 0);
-            sprintf(welcome + strlen(welcome), " has entered the chat!");
-            client_sockfd[free_addr] = new_socket;
-            for (size_t i = 0; i < MAX_BACKLOG; i++) {
-                send(client_sockfd[i], welcome, sizeof(welcome), 0);
-            }
-            free_addr++;
-            free_addr %= MAX_BACKLOG;
-        }
-        for (size_t i = 0; i < MAX_BACKLOG; i++) {
-            ssize_t recvlen = recv(client_sockfd[i], client_messages[i].msg, MAX_MSG_LENGTH - 1, 0);
-            if (strlen(client_messages[i].msg) > 0) {
-                printf("Client %d: %s\n", client_sockfd[i], client_messages[i].msg);
-            }
-            if (recvlen < 0) {
-                printf("Error\n");
-            }
-        }
-    }
+    // while (run) {
+    //     int new_socket = accept(server_sockfd, (struct sockaddr *) &sa_addr, client_addr_sizes + free_addr);
+    //     if (!run) break;
+    //     if (new_socket >= 0) {
+    //         printf("ACCEPTED\n");
+    //         recv(new_socket, welcome, MAX_CLIENT_NAME_SIZE - 1, 0);
+    //         sprintf(welcome + strlen(welcome), " has entered the chat!");
+    //         client_sockfd[free_addr] = new_socket;
+    //         for (size_t i = 0; i < MAX_BACKLOG; i++) {
+    //             send(client_sockfd[i], welcome, sizeof(welcome), 0);
+    //         }
+    //         free_addr++;
+    //         free_addr %= MAX_BACKLOG;
+    //     }
+    //     for (size_t i = 0; i < MAX_BACKLOG; i++) {
+    //         ssize_t recvlen = recv(client_sockfd[i], client_messages[i].msg, MAX_MSG_LENGTH - 1, 0);
+    //         if (strlen(client_messages[i].msg) > 0) {
+    //             printf("Client %d: %s\n", client_sockfd[i], client_messages[i].msg);
+    //         }
+    //         if (recvlen < 0) {
+    //             printf("Error\n");
+    //         }
+    //     }
+    // }
+    while (run) {}
 
     for (size_t i = 0; i < MAX_BACKLOG; i++) {
         shutdown(i, SHUT_RDWR);
